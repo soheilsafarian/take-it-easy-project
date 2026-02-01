2378007201a044946a49796e40cefb28603f5576
sohisafa <soheilsafarian8@gmail.com>
2026-02-02 00:11:42 +0100

    Initial Maven setup with Main and first test

---
+++ src/test/java/it/units/takeiteasy/MainTest.java
@@ -0,0 +1,12 @@
+package it.units.takeiteasy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
